d5c6d942db98cf366801533fdf4c89add6797e9e
Aubry Cholleton <aubry.cholleton@epfl.ch>
2013-12-15 03:01:34 +0100

    Encapsulate hiddenMarkovModel (to later implement better serialization in DB)

---
+++ app/models/HiddenMarkovModel.java
@@ -0,0 +1,87 @@
+package models;
+
+public class HiddenMarkovModel {
+
+  private Hmm<ObservationVector> hiddenMarkovModel;
+
+  public HiddenMarkovModel(
+    List<List<ObservationVector>> observations,
+    int nbGaussians,
+    int nbFeatures,
+    int nbStates
+  ) {
+
+    KMeansLearner<ObservationVector> kml =
+      new KMeansLearner(
+        nbStates,
+        new OpdfMultiGaussianMixtureFactory(nbGaussians, nbFeatures),
+        observations);
+
+    hiddenMarkovModel = kml.iterate();
+    
+    hiddenMarkovModel.setPi(0, 1);
+    for(int i=1; i<nbStates; i++) {
+      hiddenMarkovModel.setPi(i, 0);
+    }
+
+    for(int i=0; i<(nbStates); i++) {
+      for(int j=0; j<(nbStates); j++) {
+        hiddenMarkovModel.setAij(i,j, 0);
+      }
+    }
+    for(int i=0; i<(nbStates-1); i++) {
+      hiddenMarkovModel.setAij(i,i, 0.5);
+      hiddenMarkovModel.setAij(i,i+1, 0.5);
+    }
+
+    hiddenMarkovModel.setAij(nbStates-1,nbStates-1, 0.5);
+
+    System.out.println(hiddenMarkovModel.toString());
+  }
+
+  public boolean train(List<List<ObservationVector>> observations) {
+    
+    init();
+
+    System.out.println("Training...");
+
+    BaumWelchScaledLearner bwl = new BaumWelchScaledLearner();
+
+    try {
+      hiddenMarkovModel = bwl.learn(hiddenMarkovModel.clone(), observations);
+    } catch(CloneNotSupportedException e) {
+      System.out.println("Impossible to clone this HMM.");
+    }
+
+    return true;
+
+  }
+
+  public double probability(List<ObservationVector> observation) {
+    
+    return hiddenMarkovModel.lnProbability(observation);
+  }
+
+  public String getHiddenMarkovModel(){
+    HmmWriter hmmWriter = new HmmWriter();
+    Writer writer = new StringWriter();
+    OpdfMultiGaussianMixtureWriter oPdfWriter = new OpdfMultiGaussianMixtureWriter();
+    try {
+      hmmWriter.write(writer, oPdfWriter, hiddenMarkovModel);
+    } catch (IOException e) {
+      System.out.println("Oops ! An exception occured while saving the HMM.");
+    }
+    return writer.toString();
+  }
+
+  public void setHiddenMarkovModel(String hmm){
+    HmmReader hmmReader = new HmmReader();
+    Reader reader = new StringReader(hmm);
+    OpdfMultiGaussianMixtureReader oPdfReader = new OpdfMultiGaussianMixtureReader();
+    try {
+      hiddenMarkovModel = hmmReader.read(reader, oPdfReader);
+    } catch (Exception e) {
+      System.out.println("Oops ! An exception occured while reading the HMM.");
+    }
+  }
+}
