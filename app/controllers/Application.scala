c5ac36efe6c602f2277566042b69a168f7b24cd7
Aubry Cholleton <aubry.cholleton@epfl.ch>
2014-01-02 18:12:35 +0100

    Add index page.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Signature Verification System"))
+    Ok(views.html.index())
   }
 
   def javascriptRoutes = Action { implicit request =>
